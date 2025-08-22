c816a3a1b1d941707def2304642db5093e1b7b89
DevTimmy <oyinloyematthew6@gmail.com>
2025-08-22 01:58:06 +0100

    Feat: Added the gradient

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,9 +5,11 @@ import group from "/src/assets/Group.png"
 
 
 const HeroSection = () => {
-  return (
-    <div className="flex flex-col justify-center items-center py-12 px-4 gap-6 min-h-screen bg-multi-gradient">
-          <div className="bg-white rounded-full shadow-sm px-4 py-1">
+    return (
+        <div className="flex flex-col justify-center items-center py-12 px-4 gap-6 min-h-screen" style={{background: 
+            `radial-gradient(ellipse 100% 200% at center 97%, #d8caeaff 0%, rgba(216, 209, 224, 0.6) 25%,
+                         rgba(151, 71, 255, 0.2) 50%, transparent 70%), white`}}>
+          <div className="bg-white rounded-full shadow-[0 4px 20px rgba(0, 0, 0, 0.05)] px-4 py-1">
               <h3 className="font-alexandria font-light text-[#002B6B] py-1 px-2 flex gap-4"><img src={group} />Where Cloud Builders Unite</h3>
           </div>
           <div>
@@ -17,7 +19,7 @@ const HeroSection = () => {
           <div>
               <p className="text-[#414D60] text-center">Empowering Students With AWS Technology</p>
           </div>
-          <div className="flex flex-col md:flex-row gap-4 md:gap-12">
+          <div className="flex flex-col md:flex-row gap-4 md:gap-12 w-3/4 items-center justify-center">
               <button className="bg-[#9747FF] py-2 px-5 rounded-full text-white flex gap-3">Join Meet Up <img className="bg-white rounded-full" src={arrow } /></button>
               <button className="bg-[#9747FF] py-2 px-5 rounded-full text-white flex gap-3">Join Community <img className="bg-white rounded-full" src={arrow } /></button>
           </div>
