f004c3e8a518b63c427f7e9c8819f18a54198338
DevTimmy <oyinloyematthew6@gmail.com>
2025-08-24 10:00:43 +0100

    Feat: Started the Event section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import HeroSection from './components/HeroSection.tsx';
+import Event from './components/Event.tsx'
 
 function App() {
   return (
     <>
       <HeroSection />
+      <Event />
     </>
   );
 }
