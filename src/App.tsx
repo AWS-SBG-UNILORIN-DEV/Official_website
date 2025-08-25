bda7265d8095e1f4cc63aa95e0647181e17c5615
sweathannah <sultanatmbashir@gmail.com>
2025-08-25 18:27:11 +0100

    added animation to about us section and started community section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import HeroSection from './components/HeroSection.tsx';
 import AboutSection from './components/About.tsx';
+import CommunitySection from './components/community.tsx';
 
 function App() {
   return (
     <>
       <HeroSection />
       <AboutSection />
+      <CommunitySection/>
     </>
   );
 }
---
+++ src/components/community.tsx
@@ -0,0 +1,11 @@
+import { motion } from "framer-motion";
+
+const CommunitySection = () => {
+  return (
+    <section className="bg-[#FFFFFF] lg:my-[5rem] my-[3rem] lg:mx-[6.4rem] mx-[2rem] font-alexandria text-center items-center gap-[2.8rem] ">
+        
+    </section>
+  );
+};
+
+export default CommunitySection;
