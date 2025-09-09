e3ee9f6cdb91bfc86e882aec0b3d5f21bfffead9
DevTimmy <oyinloyematthew6@gmail.com>
2025-09-09 01:04:23 +0100

    Feat: Done With the Faq Section

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import accordionBtn from '/Star1.png';
-// import FaqData from '/src/assets/faqData.json';
+import { motion } from 'motion/react';
 
 // main component type checking
 interface Faq {
@@ -13,50 +13,16 @@ interface FaqType {
   FaqData: Faq[];
 }
 
-// Accordion function type checking
-// interface Accordions {
-//   title: string;
-//   content: string;
-//   isOpen: boolean;
-//   onToggle: boolean;
-// }
-
-// interface AccordionType {
-//   Accordion: Accordions[];
-// }
-
-// const AccordionItem = ({ Accordion }: AccordionType, { FaqData }: FaqType) => {
-//   console.log(FaqData);
-//   return (
-//     <div
-//       className={`bg-white rounded-3xl overflow-hidden transition-all duration-300 ${isOpen ? 'max-h-96' : 'max-h-20'}`}
-//     >
-//       <div className='flex justify-between item-start p-6 cursor-pointer'>
-//         <div className='text-2xl font-bold'></div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// AccordionItem(FaqData, FaqData);
-// width: 56;
-// height: 56;
-// angle: 0 deg;
-// opacity: 1;
-// border-radius: 48px;
-
+// const Faq = ({ FaqData }: FaqType) => {
 const Faq = ({ FaqData }: FaqType) => {
-  console.log(FaqData);
-  const [isOpen, setIsOpen] = useState(null);
+  const [isOpen, setIsOpen] = useState<string | null>(null);
 
-  // useEffect(() => {}); 
-
-  const toggleOpen = (id: string) => {
-    setIsOpen(isOpen === id ? null : id)
-  }
+  const onClick = (id: string) => {
+    setIsOpen(isOpen === id ? null : id);
+  };
 
   return (
-    <div className=' py-12 px-8 gap-6 min-h-screen '>
+    <div className=' py-12 px-8 gap-6 min-h-screen flex items-center justify-center'>
       <div className='flex flex-col justify-center items-start gap-8 w-full lg:w-[90%]'>
         <div>
           <h2 className='w-full text-left font-normal text-2xl'>
@@ -64,23 +30,38 @@ const Faq = ({ FaqData }: FaqType) => {
           </h2>
         </div>
         <div className='w-full grid grid-cols-1 lg:grid-cols-2 gap-4'>
+          {/* <div className='w-full flex  gap-4 lg:w-[40%] lg:flex-row'> */}
           {FaqData.map((item, id) => (
-            <div key={id} className=''>
-              <div className='w-full md:w-3/4 lg:w-full opacity-100 gap-4 rounded-[20px] border border-[1px] p-6 shadow border border-gray-300 cursor-pointer'>
-                <div className='flex justify-around items-start'>
+            <div key={id} onClick={() => onClick(item.id)}>
+              <div
+                className={`w-full md:w-full lg:w-full opacity-100 gap-4 rounded-[20px] border border-[1px] p-6 shadow border border-gray-300 cursor-pointer ${isOpen === item.id ? 'bg-[#8AB9FF]/30 shadow-[0_4px_20px_rgba(138,185,255,0.4)]' : 'bg-[#FFFFFF]'}`}
+              >
+                <div className='flex justify-around items-start gap-6'>
                   <h1 className='w-[36px] h-[36px] bg-[#EDEEF0] rounded-full text-center flex items-center justify-center'>
                     {item.id}
                   </h1>
                   <h3 className='font-alike font-light text-md mb-4 md:mb-0 lg:text-xl flex itemx-center justify-center'>
                     {item.question}
                   </h3>
-                  <img src={accordionBtn} className='pt-2 cursor-pointer' />
+                  <img
+                    src={accordionBtn}
+                    className={`pt-2 cursor-pointer ${isOpen === item.id ? 'rotate-24' : ''}`}
+                  />
                 </div>
-                <div className='hidden'>
-                  <p className='font-alike font-light text-md mb-4 md:mb-0 lg:text-md'>
-                    {item.answer}
-                  </p>
-                </div>
+                {isOpen === item.id && (
+                  <motion.div
+                    // className={`transition-all duration-500 ease-in-out overflow-hidden ${isOpen === item.id ? 'max-h-40 opacity-100' : 'max-h-0 opacity-0'}`}
+                    key={item.id}
+                    initial={{ opacity: 0, height: 0 }}
+                    animate={{ opacity: 1, height: 'auto' }}
+                    exit={{ opacity: 0, height: 0 }}
+                    transition={{ duration: 0.4, ease: 'easeInOut' }}
+                  >
+                    <p className='font-alike font-light px-15 md:px-20 text-sm mb-4 md:mb-0 lg:text-md lg:px-10 pt-4 leading-loose'>
+                      {item.answer}
+                    </p>
+                  </motion.div>
+                )}
               </div>
             </div>
           ))}
@@ -91,4 +72,3 @@ const Faq = ({ FaqData }: FaqType) => {
 };
 
 export default Faq;
-
